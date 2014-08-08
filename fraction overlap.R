36eb3f1b1fdad0141f84357a70cbd20b9b993ca5
bengalengel <bengelmann9@gmail.com>
2014-08-07 21:54:26 -0500

    added fraction scripts

---
+++ fraction overlap.R
@@ -0,0 +1,15 @@
+
+##count the number of peptides that are found in adjacent fractions.
+
+peptides <- read.table("./MQ output/7_17_output/peptides.txt", sep = "\t", header=T, fill = T)
+
+
+##some sort of nested for loop.
+#for each observation
+#for each slice
+#if a value is present AND a value is present in the next slice
+#add a value to count
+
+slice, add to count if identi
+table(peptides$Slice.1)
+head(peptides$Slice.1)
