62a69722ba360794f9ebc6857426fa5500cf3f4a
bengalengel <bengelmann9@gmail.com>
2014-10-30 11:32:34 -0500

    update R script added

---
+++ update R script.R
@@ -0,0 +1,4 @@
+# installing/loading the package:
+if(!require(installr)) { install.packages("installr"); require(installr)} #load / install+load installr
+
+updateR(F, T, T, F, T, F, T) # install, move, update.package, quit R.
