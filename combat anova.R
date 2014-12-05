310b605ac0589209a2d8fded924e887e56caac09
bengalengel <bengelmann9@gmail.com>
2014-12-05 12:06:57 -0600

    combat anova 2

--- combat anova.R
+++ combat anova.R
@@ -40,12 +40,67 @@ melted$techrep <- techrep
 # for(i in 1:1562)
 test <- melted[(melted$Var1 %in% melted$Var1[3]),]
 
-#test$biorep <- as.factor(c(1,1,2,2,3,3,4,4,5,5,6,6))
-#test$techrep <- as.factor(c(1:12))
+test$biorep <- as.factor(c(1,1,2,2,3,3,4,4,5,5,6,6))
+test$techrep <- as.factor(c(1:12))
 # test$techrep <- as.factor(c(1:12))
 
 test$biorep <- as.factor(c(1,1,2,2,1,1,2,2,1,1,2,2))
 test$techrep <- as.factor(c(1,2,1,2,3,4,3,4,5,6,5,6))
+
+
+
+library(nlme)
+lme.mod1 <- lme(value ~ individual, random=~1|individual/biorep/techrep, test)
+
+rsquared.glmm(lme.mod1)
+
+#Change to old optimizer to solve convergence issueslme4.models2[2:4, -(1:3)]
+lme.mod1.1 <- lme(y ~ fixed1, random=~fixed1|rand2/rand1, control = lmeControl(opt = "optim"), data) 
+lme.mod2 <- lme(y ~ fixed1 + fixed2, random=~1|rand2/rand1, data)
+(lme.models <- rsquared.glmm(list(lme.mod1, lme.mod1.1, lme.mod2)))
+# Compare to lme4 models, minor differences
+all.equal(lme4.models2[2:4, -(1:3)], lme.models[,-(1:3)], tol = 1e-4)
+
+
+library(lme4)
+value <- test$value
+individual <- test$individual
+biorep <- test$biorep
+techrep <- test$techrep
+
+model<-lmer(value~individual +(1|individual/biorep)) 
+
+model<-lmer(y~treatment+(1|treatment/mouse),family=binomial) 
+
+test$value ~ test$individual/test$biorep/test$techrep
+
+
+
+
+
+library(nlme)
+lme.mod1 <- lme(y ~ fixed1, random=~1|rand2/rand1, data)
+#Change to old optimizer to solve convergence issueslme4.models2[2:4, -(1:3)]
+lme.mod1.1 <- lme(y ~ fixed1, random=~fixed1|rand2/rand1, control = lmeControl(opt = "optim"), data) 
+lme.mod2 <- lme(y ~ fixed1 + fixed2, random=~1|rand2/rand1, data)
+(lme.models <- rsquared.glmm(list(lme.mod1, lme.mod1.1, lme.mod2)))
+# Compare to lme4 models, minor differences
+all.equal(lme4.models2[2:4, -(1:3)], lme.models[,-(1:3)], tol = 1e-4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #nested/hierarchical anova
 res1 <- lm(test$value ~ test$individual/test$biorep/test$techrep)
