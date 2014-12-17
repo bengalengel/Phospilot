7c7b89a8d638b9c99ff5110ca646b6fa637d5a5a
bengalengel <bengelmann9@gmail.com>
2014-12-17 17:15:18 -0600

    more variance estimation

--- variance estimates.R
+++ variance estimates.R
@@ -1,9 +1,10 @@
 # pulling variance component estimates from lme model using only the paired design. Some convergence issues.
-
+library(nlme)
 
 #all random model with a fixed intercept.technical replicate not explicitly modeled. 
 sites <- c()
 Varcomp <- c()
+#ctrl <- lmeControl(opt='optim') with this arguement nothing converges? throws an error
 for(i in 1:1562){
   test <- melted[(melted$Var1 %in% melted$Var1[i]),]
   test1 <- test[3:6]
@@ -16,11 +17,78 @@ for(i in 1:1562){
 }
 colnames(Varcomp) <- sites
 row.names(Varcomp) <- c("individual","biorep","residual")
+dim(Varcomp)
 boxplot(log10(t(Varcomp)))
 summary(t(Varcomp))
+head(t(Varcomp))
+
+#random nested with fixed individual. with optim arguement low variances for biorep are gone.
+sites <- c()
+Varcomp <- c()
+ctrl <- lmeControl(opt='optim')
+for(i in 1:1562){
+  test <- melted[(melted$Var1 %in% melted$Var1[i]),]
+  test1 <- test[3:6]
+  lmemodel <- lme(value~individual, control = ctrl, data=test1, random =~1|individual/biorep)
+  temp <- as.numeric(VarCorr(lmemodel)[,1])
+  temp <- temp[!is.na(temp)]
+  temp <- na.omit(temp)
+  Varcomp <- cbind(Varcomp,temp)
+  sites <- c(sites,as.character(unique(test$Var1)))
+}
+colnames(Varcomp) <- sites
+row.names(Varcomp) <- c("individual","biorep","residual")
+dim(Varcomp)
+boxplot(log10(t(Varcomp)))
+summary(t(Varcomp))
+head(t(Varcomp))
 
 
-#all random model with a fixed intercept.technical replicate explicitly modeled. 
+#random nested with fixed individual.leave optim out
+sites <- c()
+Varcomp <- c()
+ctrl <- lmeControl(opt='optim')
+for(i in 1:1562){
+  test <- melted[(melted$Var1 %in% melted$Var1[i]),]
+  test1 <- test[3:6]
+  lmemodel <- lme(value~individual, data=test1, random =~1|individual/biorep)
+  temp <- as.numeric(VarCorr(lmemodel)[,1])
+  temp <- temp[!is.na(temp)]
+  temp <- na.omit(temp)
+  Varcomp <- cbind(Varcomp,temp)
+  sites <- c(sites,as.character(unique(test$Var1)))
+}
+colnames(Varcomp) <- sites
+row.names(Varcomp) <- c("individual","biorep","residual")
+dim(Varcomp)
+boxplot(log10(t(Varcomp)))
+summary(t(Varcomp))
+head(t(Varcomp))
+
+#random nested with fixed individual/biorep. no optim arguement because with it things fail to converge.
+sites <- c()
+Varcomp <- c()
+#ctrl <- lmeControl(opt='optim')
+for(i in 1:1562){
+  test <- melted[(melted$Var1 %in% melted$Var1[i]),]
+  test1 <- test[3:6]
+  lmemodel <- lme(value~individual/biorep, data=test1, random =~1|individual/biorep)
+  temp <- as.numeric(VarCorr(lmemodel)[,1])
+  temp <- temp[!is.na(temp)]
+  temp <- na.omit(temp)
+  Varcomp <- cbind(Varcomp,temp)
+  sites <- c(sites,as.character(unique(test$Var1)))
+}
+colnames(Varcomp) <- sites
+row.names(Varcomp) <- c("individual","biorep","residual")
+dim(Varcomp)
+boxplot(log10(t(Varcomp)))
+summary(t(Varcomp))
+head(t(Varcomp))
+
+
+
+#all random model with a fixed intercept.technical replicate explicitly modeled. NO DF FOR TECH REP ESTIMATE!
 sites <- c()
 Varcomp <- c()
 ctrl <- lmeControl(opt='optim')
