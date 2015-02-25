4a284dfa5fa010747327f06c4e3a2b3176bcf6b1
bengalengel <bengelmann9@gmail.com>
2015-02-24 20:58:46 -0600

    snp annotations file

--- snp annotations.R
+++ snp annotations.R
@@ -5,10 +5,10 @@
 # The next goal is to identify those sites with missing phosphorylated residues or regions surrounding the residues to get a sense of effect size after normalizing by protein.
 
 #import yannick files
-yannick18486 <- read.table("C:/Users/Brett/Desktop/pilot_18486_allNS.csv",sep = ',', header = T)
-yannick18862 <- read.table("C:/Users/Brett/Desktop/pilot_18862_allNS.csv",sep = ',', header = T)
-yannick19160 <- read.table("C:/Users/Brett/Desktop/pilot_19160_allNS.csv",sep = ',', header = T)
-
+yannick18486 <- read.table("C:/Users/Brett/Desktop/pilot_18486_allNS.csv", sep = ',', header = T)
+yannick18862 <- read.table("C:/Users/Brett/Desktop/pilot_18862_allNS.csv", sep = ',', header = T)
+yannick19160 <- read.table("C:/Users/Brett/Desktop/pilot_19160_allNS.csv", sep = ',', header = T)
+pqtl <- read.table("E:/My Documents/Dropbox/Postdoc-Gilad/Yannick SNP Phos/1260793_DatafileS1_pQTLs.csv", sep = ',', header = T)
 
 #add a +/- based on UNIPROT.ID annotation, also add if isoform is present. Did I really add the annotation even if there is an
 # isoform? NO see below
@@ -133,6 +133,9 @@ multExpanded1_withDE$Ind18486_SNPisofull = ifelse(substr(multExpanded1_withDE$Pr
 multExpanded1_withDE$Ind18862_SNPisofull = ifelse(substr(multExpanded1_withDE$Protein,1,6) %in% yannick18862full$UNIPROT.ID, "+","-")
 multExpanded1_withDE$Ind19160_SNPisofull = ifelse(substr(multExpanded1_withDE$Protein,1,6) %in% yannick19160full$UNIPROT.ID, "+","-")
 
+#add the phosphomotif information
+
+
 
 anysnp <- multExpanded1_withDE[multExpanded1_withDE$Ind18486_SNPisofull == "+" | multExpanded1_withDE$Ind18862_SNPisofull == "+" | multExpanded1_withDE$Ind19160_SNPisofull == "+",]
 #make background as data frame and add snp annotation
@@ -180,7 +183,84 @@ colSums(protmatrix)
 [1]  269 1722
 #13% of the proteins have a snp (yet 34% of the phosphopeptides map to a snp protein. proteins with snp tend to be longer and therefore have more phosphorylations)
 
-******************************************now for the protein normalized data using all snps and full counts**************************
+##now for the tests of regions near the phosphosite***********************************************************888
+#Are motif altering snps enriched for Diffphos?
+
+anysnp <- multExpanded1_withDE[multExpanded1_withDE$Ind18486_SNPisofull == "+" | multExpanded1_withDE$Ind18862_SNPisofull == "+" | multExpanded1_withDE$Ind19160_SNPisofull == "+",]
+#make background as data frame and add snp annotation
+##breakdown to the level of protein for categorical enrichment analysis
+background <- subtoDE$Protein
+background <- as.data.frame(background)#background2 is the subtoDE df but with 'rev' entries removed.
+colnames(background) <- "Protein"
+background$anysnp <- ifelse(background$Protein %in% anysnp$Protein, "+","-")
+#add the site ids as a column in the data frame
+background$idmult <- subtoDE$idmult
+#add the DE in any contrast information at the site level!
+GlobalFs <- subtoDE[subtoDE$globalFsig=="+",]#1355 obs
+background$DEany <- ifelse(background$idmult %in% GlobalFs$idmult, "+", "-")
+
+#add the phosphovariant information to the anysnp dataframe, then subset this dataframe if variant present in any line
+yannick18486motifvar <- yannick18486full[yannick18486full$Phosphomotif.positive..variant.is.within.7.Amino.acids.N.or.C.terminal.to.central.S.T.Y.=="yes",]##871 observations
+yannick18862motifvar <- yannick18862full[yannick18862full$Phosphomotif.positive..variant.is.within.7.Amino.acids.N.or.C.terminal.to.central.S.T.Y.=="yes",]#487 observations
+yannick19160motifvar <- yannick19160full[yannick19160full$Phosphomotif.positive..variant.is.within.7.Amino.acids.N.or.C.terminal.to.central.S.T.Y.=="yes",]#372 observations
+###
+anysnp$Ind18486_motifproximal = ifelse(substr(anysnp$Protein,1,6) %in% yannick18486motifvar$UNIPROT.ID, "+","-")
+anysnp$Ind18862_motifproximal = ifelse(substr(anysnp$Protein,1,6) %in% yannick18862motifvar$UNIPROT.ID, "+","-")
+anysnp$Ind19160_motifproximal = ifelse(substr(anysnp$Protein,1,6) %in% yannick19160motifvar$UNIPROT.ID, "+","-")
+#now subset to get all the 'motif'variants
+motifvars <- anysnp[anysnp$Ind18486_motifproximal == "+" | anysnp$Ind18862_motifproximal == "+" | anysnp$Ind19160_motifproximal == "+",]
+#add PROTEIN LEVEL proximal annotation to 'background'
+background$nearmotif <- ifelse(background$Protein %in% motifvars$Protein, "+", "-")#isoforms will match but weren't important for mapping
+## only 586 sites are annotated as near the motif out of 5000 mapping to 151 unique proteins
+## add pqtl information
+background$pqtl <- ifelse(substr(background$Protein,1,6) %in% pqtl$UNIPROT.ID, "+","-")## got 168/278!!
+
+#enrichment of motif proximal variants at the protein level
+#create the first row of the contingency matrix
+row1 <- c(nrow(background[background$DEany=="+" & background$nearmotif == "+",]), nrow(background[background$DEany == "+" & background$nearmotif == "-",])) 
+#second row
+row2 <- c(nrow(background[background$DEany=="-" & background$nearmotif == "+",]), nrow(background[background$DEany == "-" & background$nearmotif == "-",])) 
+
+# contmatrix
+# [,1] [,2]
+# row1  181 1173
+# row2  405 3232
+
+#FEtest
+contmatrix <- rbind(row1,row2)
+result <- fisher.test(contmatrix, alternative = "g")
+result$p.value
+##just barely significant .01743108 
+
+##Note that this assessment asks the question "are proteins with motif variants more likely than those without motif variants to be differentially phosphorylated?"
+
+##"this is different than: "are phosphosites with a modification surrounding the residue more likely to be DiffPhos than those without a proximal modification?"
+
+# The answers to these two questions speaks to the nature of PTM crosstalk.
+
+#enrichment of pqtls in diff phos!!
+
+#create the first row of the contingency matrix
+row1 <- c(nrow(background[background$DEany=="+" & background$pqtl == "+",]), nrow(background[background$DEany == "+" & background$pqtl == "-",])) 
+#second row
+row2 <- c(nrow(background[background$DEany=="-" & background$pqtl == "+",]), nrow(background[background$DEany == "-" & background$pqtl == "-",])) 
+
+# [,1] [,2]
+# row1   50 1304
+# row2  118 3519
+
+#FEtest
+contmatrix <- rbind(row1,row2)
+result <- fisher.test(contmatrix, alternative = "g")
+result$p.value
+## Not significant! 0.2422782 this is just "is a protein that has a pqtl (that may not be different across individuals) enriched in DiffPhos"
+
+##I need "are pqtls - where there is a difference in alleles between individuals in this subset - enriched in DE subset?"
+
+
+
+
+#******************************************now for the protein normalized data using all snps and full counts**************************
 ##subset to those phosphoobservations subject to DE (the background set of possibilities)
 subtoDEpn <- multExpanded1_withDE[multExpanded1_withDE$SubtoDEpn == "+",]#3603
 subtoDEpn <- subtoDEpn[!grepl("REV",subtoDEpn$Protein),]#now removed REVerse hits and still 3603
@@ -198,7 +278,18 @@ background$idmult <- subtoDEpn$idmult
 GlobalFs <- subtoDEpn[subtoDEpn$globalFsigpn=="+",]#972 obs with this truncated set
 background$DEany <- ifelse(background$idmult %in% GlobalFs$idmult, "+", "-")
 
-#create the first row of the contingency matrix
+#add the variant proximal annotation neglecting isoform information
+anysnp$Ind18486_motifproximal = ifelse(substr(anysnp$Protein,1,6) %in% yannick18486motifvar$UNIPROT.ID, "+","-")
+anysnp$Ind18862_motifproximal = ifelse(substr(anysnp$Protein,1,6) %in% yannick18862motifvar$UNIPROT.ID, "+","-")
+anysnp$Ind19160_motifproximal = ifelse(substr(anysnp$Protein,1,6) %in% yannick19160motifvar$UNIPROT.ID, "+","-")
+#now subset to get all the 'motif'variants
+motifvars <- anysnp[anysnp$Ind18486_motifproximal == "+" | anysnp$Ind18862_motifproximal == "+" | anysnp$Ind19160_motifproximal == "+",]
+
+#add PROTEIN LEVEL proximity annotation to 'background'
+background$nearmotif <- ifelse(background$Protein %in% motifvars$Protein, "+", "-")#isoforms will match but weren't important for mapping
+
+
+#create the first row of the contingency matrix to test enrichment of snp containing proteins in DE subset.
 row1 <- c(nrow(background[background$DEany=="+" & background$anysnp == "+",]), nrow(background[background$DEany == "+" & background$anysnp == "-",])) 
 
 row2 <- c(nrow(background[background$DEany=="-" & background$anysnp == "+",]), nrow(background[background$DEany == "-" & background$anysnp == "-",])) 
@@ -214,11 +305,22 @@ result <- fisher.test(contmatrix, alternative = "g")
 result$p.value
 ## 0.0009148777
 
-
 ##now for the tests of regions near the phosphosite
-Are motif altering snps enriched for Diffphos?
+#create the first row of the contingency matrix
+row1 <- c(nrow(background[background$DEany=="+" & background$nearmotif == "+",]), nrow(background[background$DEany == "+" & background$nearmotif == "-",])) 
+#second row
+row2 <- c(nrow(background[background$DEany=="-" & background$nearmotif == "+",]), nrow(background[background$DEany == "-" & background$nearmotif == "-",])) 
+#FEtest
+contmatrix <- rbind(row1,row2)
+result <- fisher.test(contmatrix, alternative = "g")
+result$p.value
+##not significant this time.
+## 0.4805825 
 
-
+# > contmatrix
+# [,1] [,2]
+# row1  122  850
+# row2  327 2304
 
 
 
