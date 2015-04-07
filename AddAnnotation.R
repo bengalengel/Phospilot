e7fc1adbebb7ade3109539408dd96a6231cb4163
bengalengel <bengelmann9@gmail.com>
2015-04-07 17:14:29 -0500

    added enrichment analysis to annotation file. still concerned about mapping phosphopeptides to proteins

--- AddAnnotation.R
+++ AddAnnotation.R
@@ -324,84 +324,168 @@ return(multExpanded1_withDE)
 
 # Enrichment. Will be a separate function
 ##########################
-#for now not worrying about multiple phosphorylations and such.
 
-backgroundGO <- multExpanded1_withDE[multExpanded1_withDE$SubtoDE=="+",]
+#First is GO. 
+
+#get background and correct for multiple observations of the same peptides (multiplicities)
+backgroundGO <- multExpanded1_withDE[multExpanded1_withDE$SubtoDE == "+",]
+#remove duplicate entries of the same peptide (multiplicities). Subset by unique id. id refers to a phosphosite
+backgroundGO <- backgroundGO[!duplicated(backgroundGO$id),]#the duplicated returns a logical identifying the first instance of duplication from an ordererd query. Therefore it is safe to subset using 'duplicated'
 backgroundGO <- backgroundGO$GOIDs
-x <- sapply(backgroundGO, function(x) strsplit(x, ";"))
-x <- as.vector(x)
-x <- unlist(x)
-x <- x[x!='NA']
-#unique protein groups require annotation assignment
-#   names(GOIDs)[i] <- proteins[i]
-#   uniIDs <- strsplit(proteins[i], ";")
-#   uniIDs <- as.character(unlist(uniIDs))
-#   uniIDs <- substr(uniIDs,1,6)#de-isoform becuase if not it throws an error
+BGGO <- sapply(backgroundGO, function(x) strsplit(x, ";"))
+BGGO <- unlist(BGGO)
+BGGO <- BGGO[BGGO!='NA']#to be passed
 
+enrichedGO <- multExpanded1_withDE[multExpanded1_withDE$globalFsig == "+",]
+enrichedGO <- enrichedGO[!duplicated(enrichedGO$id),]
+enrichedGO <- enrichedGO$GOIDs
+DEGO <- sapply(enrichedGO, function(y) strsplit(y, ";"))
+DEGO <- unlist(DEGO)
+DEGO <- DEGO[DEGO!='NA']#to be passed
+
+#now for the protein normalized DE
+backgroundGO <- multExpanded1_withDE[multExpanded1_withDE$SubtoDEpn =="+",]
+#remove duplicate entries of the same peptide (multiplicities). Subset by unique id. id refers to a phosphosite
+backgroundGO <- backgroundGO[!duplicated(backgroundGO$id),]#the duplicated returns a logical identifying the first instance of duplication from an ordererd query. Therefore it is safe to subset using 'duplicated'
+backgroundGO <- backgroundGO$ppGOIDs
+BGGOpn <- sapply(backgroundGO, function(x) strsplit(x, ";"))
+BGGOpn <- unlist(BGGOpn)
+BGGOpn <- BGGOpn[BGGOpn!='NA']#to be passed
+
+enrichedGO <- multExpanded1_withDE[multExpanded1_withDE$globalFsigpn == "+",]
+enrichedGO <- enrichedGO[!duplicated(enrichedGO$id),]
+enrichedGO <- enrichedGO$GOIDs
+DEGOpn <- sapply(enrichedGO, function(y) strsplit(y, ";"))
+DEGOpn <- unlist(DEGOpn)
+DEGOpn <- DEGOpn[DEGOpn!='NA']#to be passed
 
 
 
 Enrich <- function(x,y){
   #This function accepts lists of kinases found in a DE subset and background subset identified by the networkin program and return a list of adjusted pvalues for categorical enrichment using a fisher's exact test.x=background and y=enriched
-  
+  require(plyr)
   BGtable <- as.matrix(table(x))#the parenthetical should be a factor vector of ids passed to this function
   #remove entries with 0
-  BGtable <- BGtable[BGtable!=0,,drop=F]#sum of table 2 is 4643
+  BGtable <- BGtable[BGtable!=0,,drop=F]
   DEtable <- as.matrix(table(y))#the parenthetical should be a passed DE factor vector of networKin output
   DEtable <- as.matrix(DEtable[row.names(DEtable) %in% row.names(BGtable),])#removing zeros and all factors not present in BG data
   #note the use of the %in% statement to control for rownames in DeTable but not in background
   #subset the background table in a similar way to ensure we are making the proper comparisons
   BGtable <- as.matrix(BGtable[row.names(BGtable) %in% row.names(DEtable),])
   NotDE <- BGtable-DEtable
-  facttemp <- as.factor(row.names(DEtable))###########not quite working as a factor
+  facttemp <- as.factor(row.names(DEtable))
   pvals <- c()
+  frequency <- c()
+  ids <- c()
   #for each unique kinase in the DE1 kinases table I need to make a contingency table
   for(i in levels(facttemp)){#this would be the DE dataframe
     #make the first row of the contingency table
-    if(DEtable[as.character(i),] & NotDE[as.character(i),] >= 0){
+    if(DEtable[as.character(i),] & NotDE[as.character(i),] >= 0){#this condition should always be true
       DErow <- c(DEtable[as.character(i),],(sum(DEtable)-DEtable[as.character(i),]))
       NotDErow <- c(NotDE[as.character(i),],(sum(NotDE)-NotDE[as.character(i),]))
       contmatrix <- rbind(DErow,NotDErow)
       tmp <- fisher.test(contmatrix, alternative = "g")
       pvals <- c(pvals,tmp$p.value)
+      ids <- c(ids,i)
+      tmp2 <- paste(as.character(contmatrix[1,1]), as.character(colSums(contmatrix)[1]), sep = "/")
+      frequency <- c(frequency, tmp2)
     }
   }
   ##multiple testing correction for pvals
   adjps <- p.adjust(pvals,method="BH")#none pass significance in this instance
-  return(adjps)
+  #put into a dataframe object, sort by adjusted p values
+  DF <- data.frame('ID' = ids, 'Frequency' = frequency, 'pvalue' = pvals, 'adjpvalue' = adjps)
+  DF <- arrange(DF, adjpvalue)#arrange
+  DF <- DF[DF$adjpvalue<=.05,]
+  return(DF)
 }
 
+#run the function
+GODF <- Enrich(BGGO,DEGO)#from above
+#assign the annotation
+require(GO.db)
+GOannotation <- select(GO.db, keys = as.character(GODF$ID), keytype = "GOID", columns = c("TERM", "ONTOLOGY", "DEFINITION"))
+GOenrichment <- cbind(GODF,GOannotation)
 
-##Enrich use for GO
-Background <- BGkinases$id
-Enriched1 <- DE1kinases$id
-Enriched2 <- DE2kinases$id
-Enriched3 <- DE3kinases$id
-
-Enriched1up <- DE1upkinases$id
-Enriched1down <- DE1downkinases$id
-Enriched2up <- DE2upkinases$id
-Enriched2down <- DE2downkinases$id
-Enriched3up <- DE3upkinases$id
-Enriched3down <- DE3downkinases$id
-
-
-#for testing
-#x=Background
-#y=Enriched2
-
-DE1ps <- NetEnrich(Background,Enriched1)
-DE2ps <- NetEnrich(Background,Enriched2)
-DE3ps <- NetEnrich(Background,Enriched3)
+#run the function
+GODFpn <- Enrich(BGGOpn,DEGOpn)#from above
+#assign the annotation
+require(GO.db)
+GOannotationpn <- select(GO.db, keys = as.character(GODFpn$ID), keytype = "GOID", columns = c("TERM", "ONTOLOGY", "DEFINITION"))
+GOenrichmentpn <- cbind(GODFpn,GOannotationpn)
 
 
+#the two are quite different. Note that for proteins I am using the majority protein IDs from zia's protein prep. This may be incorrect given that a protein within the maj. protein ids. group may not have the same sequence as the phosphorylated peptide...
+
+#the above is true iff the phosphopeptide is a unique sequence relative all other sequences defined by the protein group in Zia's dataset.
+
+#two steps for this
+# 1) match using 'proteins' for phoshphopeptides to majority proteins in zia's prep. multiple matches are parsed by occam's razor.
+# 2) Matching protein group from Zia's prep is pruned for proteins that don't contain the matching phosphopeptide (nonphosphorylated form)
+
+x <- GOenrichment$GOID#114
+y <- GOannotationpn$GOID#82
+
+length(intersect(y,x))#22
+length(setdiff(y,x))#60
 
 
+################################################33
+
+#same steps for reactome
+###################
+#get background and correct for multiple observations of the same peptides (multiplicities)
+backgroundReact <- multExpanded1_withDE[multExpanded1_withDE$SubtoDE=="+",]
+#remove duplicate entries of the same peptide (multiplicities). Subset by unique id. id refers to a phosphosite
+backgroundReact <- backgroundReact[!duplicated(backgroundReact$id),]#the duplicated returns a logical identifying the first instance of duplication from an ordererd query. Therefore it is safe to subset using 'duplicated'
+backgroundReact <- backgroundReact$ReactIDs
+BGRO <- sapply(backgroundReact, function(x) strsplit(x, ";"))
+BGRO <- unlist(BGRO)
+BGRO <- BGRO[BGRO!='NA']#to be passed
+
+enrichedReact <- multExpanded1_withDE[multExpanded1_withDE$globalFsig == "+",]
+enrichedReact <- enrichedReact[!duplicated(enrichedReact$id),]
+enrichedReact <- enrichedReact$ReactIDs
+DERO <- sapply(enrichedReact, function(y) strsplit(y, ";"))
+DERO <- unlist(DERO)
+DERO <- DERO[DERO!='NA']#to be passed
+
+#now for the protein normalized DE
+#get background and correct for multiple observations of the same peptides (multiplicities)
+backgroundReact <- multExpanded1_withDE[multExpanded1_withDE$SubtoDE=="+",]
+#remove duplicate entries of the same peptide (multiplicities). Subset by unique id. id refers to a phosphosite
+backgroundReact <- backgroundReact[!duplicated(backgroundReact$id),]#the duplicated returns a logical identifying the first instance of duplication from an ordererd query. Therefore it is safe to subset using 'duplicated'
+backgroundReact <- backgroundReact$ppReactIDs
+pnBGRO <- sapply(backgroundReact, function(x) strsplit(x, ";"))
+pnBGRO <- unlist(pnBGRO)
+pnBGRO <- pnBGRO[pnBGRO!='NA']#to be passed
+
+enrichedReact <- multExpanded1_withDE[multExpanded1_withDE$globalFsig == "+",]
+enrichedReact <- enrichedReact[!duplicated(enrichedReact$id),]
+enrichedReact <- enrichedReact$ppReactIDs
+pnDERO <- sapply(enrichedReact, function(y) strsplit(y, ";"))
+pnDERO <- unlist(pnDERO)
+pnDERO <- pnDERO[pnDERO!='NA']#to be passed
 
 
+#run the function
+RODF <- Enrich(BGRO,DERO)
+#assign the annotation
+require(reactome.db)
+
+#assign the annotation
+ROannotation <- select(reactome.db, keys = as.character(RODF$ID), keytype = "REACTOMEID", columns = "PATHNAME")
+ROenrichment <- cbind(RODF,ROannotation)
+
+#protein normalized data
+RODFpn <- Enrich(pnBGRO,pnDERO)
+
+#assign the annotation
+ROannotationpn <- select(reactome.db, keys = as.character(RODFpn$ID), keytype = "REACTOMEID", columns = "PATHNAME")
+ROenrichmentpn <- cbind(RODFpn,ROannotationpn)
 
 
-
+##############################33
 
 
 
