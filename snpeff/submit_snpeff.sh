21d65d060874247cf0e5074d509b351dd6f33fc2
John Blischak <jdblischak@gmail.com>
2015-05-06 12:59:08 -0500

    Include chromosome in snpeff job names.

--- snpeff/submit_snpeff.sh
+++ snpeff/submit_snpeff.sh
@@ -4,5 +4,5 @@ mkdir -p log
 
 for CHR in {1..22}
 do
-  echo "bash run_snpeff.sh /mnt/lustre/data/internal/Yoruba/IMPUTE/YRI/hg19/chr${CHR}.hg19.impute2_haps.gz results/" | qsub -l h_vmem=8g -cwd -V -N snpeff -o log -j y
+  echo "bash run_snpeff.sh /mnt/lustre/data/internal/Yoruba/IMPUTE/YRI/hg19/chr${CHR}.hg19.impute2_haps.gz results/" | qsub -l h_vmem=8g -cwd -V -N chr.${CHR}.snpeff -o log -j y
 done
