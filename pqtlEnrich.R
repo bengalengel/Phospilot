ac2a550377d7d42066ee9247290a888603c3ba61
bengalengel <bengelmann9@gmail.com>
2017-10-22 17:24:33 -0500

    remove comments

--- pqtlEnrich.R
+++ pqtlEnrich.R
@@ -36,8 +36,6 @@ nrow(pqtl) #181
 ### Assign pqtls to phosphosites ----
 
 # I will use biomart to asssign ENSGids to ENSPids and from there to the phosphosites
-
-#how many of the ensgids are in the biomart ensembl75 GRCh37.p13/hg19 repository?
 library("biomaRt")
 library("seqinr")
 
@@ -53,11 +51,10 @@ ensembl_75_pqtls <- getBM(attributes = c('ensembl_gene_id', 'ensembl_transcript_
 length(unique(pqtl$ENSG)) == length(unique(ensembl_75_pqtls$ensembl_gene_id))#no genes missing. (181 of 181)
 
 #from 181 pqtl genes, how many have at least one CCDs protein id and were therefore included in the database for the search?
-
-ccds_index <- grep(pattern = "ccds", ensembl_75_pqtls$ccds, ignore.case = T)# this requires less writing than subsetting within the DF
+ccds_index <- grep(pattern = "ccds", ensembl_75_pqtls$ccds, ignore.case = T)
 ensembl_75_pqtlscds <- ensembl_75_pqtls[ccds_index,]
 length(unique(ensembl_75_pqtlscds$ensembl_gene_id))
-#179!!! Great ;)
+#179
 
 ##subset further based on gene/transcript biotype. start with 587 entries
 #known or novel gene status index
@@ -112,37 +109,9 @@ nrow(multExpanded1_withDE_annotated[multExpanded1_withDE_annotated$GelPreppQTLPo
 
 
 
-
-###LATER ----
-# #how many ENSPids from a pQTL gene per protein group are assigned to a phosphopeptide?
-# NumMatchesProtQTL <- function(queryproteins){
-#   #This function counts the matches between queryproteins and the proteins containing a non-synonymous snp
-#   #queryproteins are proteins assigned to the peptide
-#   sum(unlist(strsplit(as.character(queryproteins), ";")) %in% ensembl_75_pqtlscds$ensembl_peptide_id)
-# }
-# 
-# #apply count function to all 'leading proteins' and 'majority protein ids'
-# multExpanded1_withDE$pQTLCount <- mapply(NumMatchesProtQTL, multExpanded1_withDE$Leading.proteins)
-# multExpanded1_withDE$GelPreppQTLCount <- mapply(NumMatchesProtQTL, multExpanded1_withDE$ppMajorityProteinIDs)
-# 
-# # This is likely due to multiple protein isoforms corresponding to a single gene. How many pQTLs correspond are mapped to multiple genes (paralogs)?
-# table(multExpanded1_withDE$pQTLCount)
-# table(multExpanded1_withDE$GelPreppQTLCount)
-
-
-
-
-
-
 # Diff Phos enrichment tests -----------------------------------------------
 
-# 1)  Test for enrichment in diffphos. background is all sites subject to DiffPhos. Foreground is omnibus F significance. Category is 'with pQTL' or without pQTL at the phosphopeptide level. That is the same protein is counted as many times as it has an identified phosphopeptide in the subtodiffphos datafram. Contingency matrix is of the form:
-
-#                            in category  not in category  
-#                    DErow
-#                    NotDErow
-# 'in' category is any majority/leading protein(s) assigned to this phosphosite has a pQTL.  
-
+# 1)  Test for enrichment in diffphos. background is all sites subject to DiffPhos. Foreground is omnibus F significance. Category is 'with pQTL' or without pQTL at the phosphopeptide level.
 
 SubtoDEGelProt <- multExpanded1_withDE_annotated[multExpanded1_withDE_annotated$GelPrepCovSubtoDE == "+",] #3257
 SubtoDEConfounded <- multExpanded1_withDE_annotated[multExpanded1_withDE_annotated$ConfoundedSubtoDE == "+",] #4738
@@ -159,15 +128,12 @@ row2 <- c(nrow(SubtoDEGelProt[SubtoDEGelProt$GelPrepCovglobalFsig == "-" & Subto
 contmatrix <- rbind(row1,row2)
 result <- fisher.test(contmatrix, alternative = "g")
 result$p.value
-# 0.08152954 (9/18/2015)
-.00384 (10/18/2015)
+# .00384 (10/18/2015)
 
 
 # Threshold independent test of association ----
 
 # Regress p-values against binary assignment of sites. Assess if significant spearman rank based correlation.
-
-# Subset to those phosphosites sub to diffphos with a pqtl containing protein where the alleles differ
 pqtl.matrix <- SubtoDEGelProt[, c("GelPreppQTLPositive", "GelPrepCovFPval")]
 
 #switch to 0/1 designation
@@ -175,37 +141,7 @@ pqtl.matrix$GelPreppQTLPositive <- ifelse(pqtl.matrix$GelPreppQTLPositive == "+"
 pqtl.matrix[] <- lapply(pqtl.matrix, as.numeric)
 
 cor(pqtl.matrix, method = "spearman")
-
-# OK lots of duplicated pvalues...This is OK using spearman rank correlation and test of significance is the way to go. enrichment p values calcultated using t distribution approximation due to sample size. the number of duplicated p-values does not have an effect.
 cor.test(pqtl.matrix[[1]], pqtl.matrix[[2]], method = "spearman", exact = F)$p.value
-[1] 0.002176044
-[1] 0.02667636 (10/18/2015)
-
-
-##test glm (later)
-pqtl.glm <- glm(GelPreppQTLPositive ~ GelPrepCovFPval, data = pqtl.matrix, family = binomial)
-pqtl.glm.log <- glm(GelPreppQTLPositive ~ log10(GelPrepCovFPval), data = pqtl.matrix, family = binomial)
-
-pqtl.glm.qb <- glm(GelPreppQTLPositive ~ GelPrepCovFPval, data = pqtl.matrix, family = quasibinomial)
-pqtl.glm.log.qb <- glm(GelPreppQTLPositive ~ log10(GelPrepCovFPval), data = pqtl.matrix, family = quasibinomial)
-pqtl.glm.log2.qb <- glm(GelPreppQTLPositive ~ log10(1-log10(GelPrepCovFPval)), data = pqtl.matrix, family = quasibinomial)
-
-#this seems to make them normal
-qqnorm(log10(1-log10(pqtl.matrix$GelPrepCovFPval)))
-
-
-# a qq plot using the unadjusted/ NOMINAL p values. Here the expectation (null) is a uniform distribution of p-values.
-# This is only useful for visualization purposes and is not a test without bootstrapping.
-
-observed <- sort(pqtl.matrix$GelPrepCovFPval)
-lobs <- -(log10(observed))
-
-#create the -log10 of the null/expected distribution of p-values. Here this is the uniform distribution:
-# See this post for an explaination
-expected.dist <- 1:length(observed)/length(observed)
-lexp <- -(log10(expected.dist))
-qqplot(lexp, lobs)
-
-
+# [1] 0.02667636 (10/18/2015)
 
 
